4b146e22365319310a26d946b5892cf7e3a964bb
KairuMaina <george.kairu@moringaschool.com>
2024-12-05 09:35:49 +0300

    worked on my function skills

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// Define saturdayFun as a function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Define mondayWork as a function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;  // Use backticks for template literals
+};
+
+// Define wrapAdjective as a function
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`; // Capitalized "You" and use backticks
+    };
+}
+
+// Export the functions for use in tests
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+};
